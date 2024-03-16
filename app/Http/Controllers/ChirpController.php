1f048ba87f17796863a23a91d31d4b9d91f9394f
Humberto Jara <54317246+bertoja@users.noreply.github.com>
2024-03-15 23:15:19 -0300

    posteo formulario

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    { 
+        return view('chirps.index',[
+            'chirps' => Chirp::with('user')->latest()->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    { 
+        $validated =  $request->validate([
+            'message' => 'required'
+        ]);
+
+        $request->user()->chirps()->create([
+            'message'=> $request->get('message')
+        ]);
+        return to_route('chirps.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
